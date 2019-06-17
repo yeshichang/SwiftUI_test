00fd845fe33337d968307ec79b695b7023359ff1
ysc <15238280098@163.com>
2019-06-17 17:47:14 +0800

    init

---
+++ SwiftUI_test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI_test
+//
+//  Created by 叶世昌 on 2019/6/17.
+//  Copyright © 2019 SC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
